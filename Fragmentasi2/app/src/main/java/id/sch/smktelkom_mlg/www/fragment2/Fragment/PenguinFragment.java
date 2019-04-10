c8c6a93fc901595f0ceb79d710a0d9fac66fb483
EnophSogi <nofela_sogi_26rpl@student.smktelkom-mlg.sch.id>
2019-04-10 07:45:44 +0700

    selesai

---
+++ Fragmentasi2/app/src/main/java/id/sch/smktelkom_mlg/www/fragment2/Fragment/PenguinFragment.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.www.fragment2.Fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import id.sch.smktelkom_mlg.www.fragment2.R;
+
+/**
+ * Created by arifin on 22/02/2018. */ public class PenguinFragment extends Fragment {
+
+    public PenguinFragment(){
+
+    }  @Override  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView =  inflater.inflate(R.layout.activity_multiple_penguin, container,false);
+
+        return rootView;  } }
